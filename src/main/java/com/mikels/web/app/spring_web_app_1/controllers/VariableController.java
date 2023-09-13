ea7fd5ba5f46711391037c58d97bb223c0eaa01e
Miguel Lara <miguel.larasj@gmail.com>
2023-09-13 17:41:16 -0500

    variables in path

---
+++ src/main/java/com/mikels/web/app/spring_web_app_1/controllers/VariableController.java
@@ -0,0 +1,32 @@
+package com.mikels.web.app.spring_web_app_1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/variables")
+public class VariableController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("title", "Send Parameters from Controller to View");
+        
+        return "variable/index";
+    }
+
+    @GetMapping("/ver/{pathVariable}")
+    public String variableString(@PathVariable String pathVariable, Model model) {
+        model.addAttribute("text", " The Parameter is:" + pathVariable);
+        return "variable/ver";
+    }
+
+    @GetMapping("/ver/{pathVariable}/{number}")
+    public String variableString(@PathVariable String pathVariable, @PathVariable Integer number, Model model) {
+
+        model.addAttribute("text", " The Parameter is: " + pathVariable + " number: " + number);
+        return "variable/ver";
+    }
+}
