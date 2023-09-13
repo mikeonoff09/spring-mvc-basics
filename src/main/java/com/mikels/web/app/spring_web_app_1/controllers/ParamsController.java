ad07e4e4ddbedbb3c4950f36bd40b0e4f467b1a8
Miguel Lara <miguel.larasj@gmail.com>
2023-09-13 14:45:07 -0500

    Added support for multiple parameters in the ParamsController.

--- src/main/java/com/mikels/web/app/spring_web_app_1/controllers/ParamsController.java
+++ src/main/java/com/mikels/web/app/spring_web_app_1/controllers/ParamsController.java
@@ -6,26 +6,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/params")
 public class ParamsController {
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "params/index";
     }
 
-    /* When using the @RequestParam anotation the url must send the parameter
-     *  /params/string?text=Hola otherwise there are parameters to add to 
-     * @rRequestParam() like the defaultValue to add a value if there isn't a 
+    /*
+     * When using the @RequestParam anotation the url must send the parameter
+     * /params/string?text=Hola otherwise there are parameters to add to
+     * 
+     * @rRequestParam() like the defaultValue to add a value if there isn't a
      * value sent, or adding the required = false, in this case you should add
      * validations for a null in the parameter
-     */
+    */
     @GetMapping("/string")
-    public String param (@RequestParam(name = "text",required = false) String text,  Model model){
+    public String param(@RequestParam(name = "text", required = false) String text, Model model) {
         model.addAttribute("textito", "Texto enviado: " + text);
         return "params/string";
     }
-    
+
+    @GetMapping("/multi-params")
+    public String param(@RequestParam String text, @RequestParam Integer number, Model model) {
+        model.addAttribute("textito", "Texto enviado: " + text + " number: '" + number + "'");
+        return "params/string";
+    }
+    @GetMapping("/multi-params-servlet")
+    public String param(HttpServletRequest request, Model model) {
+        String saludo = request.getParameter("text");
+        Integer number = null;
+        try {
+            number = Integer.parseInt(request.getParameter("number"));
+        } catch (Exception e) {
+            number = 0;
+        }
+        model.addAttribute("textito", "Texto enviado: " + saludo + ", number: '" + number + "'");
+        return "params/string";
+    }
+
 }
